0f5913aa679a02a1bc28a18280a32c53e25ff7ee
Joel Myers <jmyers@Joel-Myerss-MacBook-Pro.local>
2017-09-05 16:44:19 -0400

    debugging and adding some functionality

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+// creating express app
 var app = express();
 
+// MIDDLEWARE
+// logger
+var logger = function(req,res, next) {
+console.log('logging...');
+  next();
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use (express.static(path.join(__dirname, 'public')));
+app.use(logger);
 
 app.get('/',function(req,res) {
   res.send('working');
